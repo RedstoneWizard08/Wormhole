73124f7eb2603cf91389ac2d43d8f47726b7f740
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-03-01 16:01:32 -0800

    Placeholder & typo fix

--- app/src/routes/Instances.tsx
+++ app/src/routes/Instances.tsx
@@ -1,7 +1,12 @@
+import { invoke_proxy } from "../invoke";
 import "./Instances.scss";
 
 export const Instances = () => {
     return (
-        <div className="instances-container"></div>
+        <div className="instances-container">
+            <button type="button" onClick={() => invoke_proxy("launch")}>
+                Launch
+            </button>
+        </div>
     );
 };
