3b8c7dbd54f1444bba77f054d107e6f8cf088bd8
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-03-10 06:06:24 +0000

    Work on UI

--- app/src/routes/Home.tsx
+++ app/src/routes/Home.tsx
@@ -2,13 +2,17 @@ import "./Home.scss";
 import banner from "../assets/background_banner.png";
 
 export const Home = () => {
+    const doInstall = () => {};
+
     return (
         <div className="home-container">
             <img src={banner} alt="Space Warp Logo" className="logo" />
 
             <br />
 
-            <h1 className="title">Welcome to the Space Warp Installer</h1>
+            <h1 className="title">Welcome to Wormhole</h1>
+
+            <button type="button" className="action" onClick={doInstall}>Install SpaceWarp</button>
         </div>
     );
 };
