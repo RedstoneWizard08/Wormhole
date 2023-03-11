a4f746a7d89655186b061e5d8af7afbe61c5de12
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-03-11 23:03:43 +0000

    Fix media queries for 5 cols

--- common/src/releases.rs
+++ common/src/releases.rs
@@ -31,6 +31,7 @@ pub async fn get_latest_release_zips() -> ReleaseZips {
 
     for asset in json.assets {
         if asset.content_type.eq("application/x-zip-compressed") {
+            // This is still needed even as of SpaceWarp 0.4
             if asset.name.to_lowercase().contains("bepinex") {
                 if zips.bepinex.is_none() {
                     zips.bepinex = Some(asset.browser_download_url);
