d0a1c55a20a15930fe649d5e07572fbd71d815de
Jacob Sapoznikow <jacob1coder@gmail.com>
2023-04-12 15:13:57 +0000

    Auto redirect

---
+++ src/routes/[game]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
+
+    const gameId = parseInt($page.params.game);
+
+    goto(`/${gameId}/instances`);
+</script>
