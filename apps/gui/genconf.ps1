d4cbd9192511ba2b7824e3f4060227502264e613
Jacob Sapoznikow <jacob1coder@gmail.com>
2024-05-23 18:19:19 +0000

    hopefully ps1 works

---
+++ apps/gui/genconf.ps1
@@ -0,0 +1,14 @@
+pip3 install yq --break-system-packages
+
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+Set-Location $scriptDirectory
+
+$version = (Get-Content ../../Cargo.toml | tomlq -r '.workspace.package.version')
+$env:CARGO_MANIFEST_DIR = $scriptDirectory
+$env:CARGO_PKG_VERSION = $version
+
+rustc "conf.rs" -o conf.exe
+
+.\conf.exe
+Remove-Item conf.exe
