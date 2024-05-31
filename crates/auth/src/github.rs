2fe9b7159c8b21da1673ea47e332768d32c26bcc
Jacob Sapoznikow <jacob1coder@gmail.com>
2024-05-31 22:15:23 +0000

    auth docs

--- crates/auth/src/github.rs
+++ crates/auth/src/github.rs
@@ -1,3 +1,5 @@
+//! GitHub Device Flow implementation
+
 use std::{thread, time};
 
 use chrono::{offset::Utc, DateTime, Duration};
@@ -5,36 +7,67 @@ use reqwest::{Body, Client};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+/// Wormhole's Client ID
 pub const WORMHOLE_CLIENT_ID: &str = "d59635fddac868079384";
+
+/// How many iterations of the polling loop
 pub const POLL_ITERATIONS: i32 = 20;
+
+/// Five seconds
 pub const FIVE_SECONDS: time::Duration = time::Duration::new(5, 0);
 
+/// Poll request data
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct PollData {
+    /// The client id
     pub client_id: String,
+
+    /// The device code
     pub device_code: String,
+
+    /// The grant type
     pub grant_type: String,
 }
 
+/// Setup request data
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SetupData {
+    /// The client id
     pub client_id: String,
+
+    /// The scope(s)
     pub scope: String,
 }
 
+/// A device flow
 #[derive(Debug, Clone)]
 pub struct DeviceFlow {
+    /// The device code
     pub device_code: Option<String>,
+
+    /// The client id
     pub client_id: String,
+
+    /// The user code
     pub user_code: Option<String>,
+
+    /// The verification uri
     pub verification_uri: Option<String>,
+
+    /// The state
     pub state: State,
 }
 
+/// A credential
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct Credential {
+    /// The access token
     pub token: String,
+
+    /// When the token will expire
     pub expiry: String,
+
+    /// The refresh token
     pub refresh_token: String,
 }
 
@@ -47,6 +80,7 @@ impl Credential {
         }
     }
 
+    /// Check if the token is expired
     pub fn is_expired(&self) -> bool {
         let exp = match DateTime::parse_from_rfc3339(self.expiry.as_str()) {
             Ok(time) => time,
@@ -57,14 +91,23 @@ impl Credential {
     }
 }
 
+/// The state
 #[derive(Debug, Clone)]
 pub enum State {
+    /// Processing
     Processing(time::Duration),
+
+    /// Failure
     Failure(String),
+
+    /// Success
     Success(Credential),
+
+    /// Pending
     Pending,
 }
 
+/// Calculate the expiry time
 pub fn calculate_expiry(expires_in: i64) -> String {
     let expires_in = Duration::try_seconds(expires_in).unwrap();
     let mut expiry: DateTime<Utc> = Utc::now();
@@ -73,14 +116,21 @@ pub fn calculate_expiry(expires_in: i64) -> String {
     expiry.to_rfc3339()
 }
 
+/// An error
 #[derive(Debug, Clone)]
 pub enum FlowError {
+    /// Max iterations reached
     MaxIterationsReached,
+
+    /// Unable to fetch
     UnableToFetch,
+
+    /// Other
     Other(String),
 }
 
 impl DeviceFlow {
+    /// Create a new device flow
     pub fn new(client_id: String) -> Self {
         Self {
             client_id,
@@ -91,6 +141,7 @@ impl DeviceFlow {
         }
     }
 
+    /// Start the device flow
     pub async fn start(client_id: String) -> Result<DeviceFlow, FlowError> {
         let mut flow = DeviceFlow::new(client_id);
 
@@ -105,6 +156,7 @@ impl DeviceFlow {
         }
     }
 
+    /// Setup the device flow
     pub async fn setup(&mut self) {
         let client = Client::new();
 
@@ -144,6 +196,7 @@ impl DeviceFlow {
         }
     }
 
+    /// Poll the device flow
     pub async fn poll(&mut self) -> Result<Credential, FlowError> {
         for i in 0..POLL_ITERATIONS {
             self.update().await;
@@ -166,6 +219,7 @@ impl DeviceFlow {
         }
     }
 
+    /// Update the device flow
     pub async fn update(&mut self) {
         let client = Client::new();
 
--- crates/auth/src/lib.rs
+++ crates/auth/src/lib.rs
@@ -1,1 +1,4 @@
+#![warn(missing_docs, rustdoc::broken_intra_doc_links)]
+//! Authentication
+
 pub mod github;
